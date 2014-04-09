26e5f06207fcdeb5196d7e2a0042c27ddff738d7
Andrew Ip <andrewip@andrewip.co.mentel.com>
2014-04-09 16:10:34 -0400

    Grab first image of a post (through its content)

--- category.php
+++ category.php
@@ -35,8 +35,13 @@
 									'post_type' => 'attachment',
 									'post_mime_type' => 'image'
 								));
-								$images = reset($images);
-								echo "<img src=\"" . $images->guid . "\"/>";
+								if(empty($images)) {
+									echo <img src=\"" . catch_that_image() . "\"/>";
+								}
+								else {
+									$images = reset($images);
+									echo "<img src=\"" . $images->guid . "\"/>";
+								}
 							}
 						?>
 					</div>
--- functions.php
+++ functions.php
@@ -329,6 +329,20 @@ function truncate_string($string, $length = 50) {
 
 }
 
+// Get first image in a post from its content
 
+function catch_that_image() {
+	global $post, $posts;
+	$first_img = '';
+	ob_start();
+	ob_end_clean();
+	$output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
+	$first_img = $matches[1][0];
+
+	if(empty($first_img)) {
+		$first_img = "/path/to/default.png";
+	}
+	return $first_img;
+}
 
 ?>
