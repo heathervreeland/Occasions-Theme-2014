493ea8f555531af9231e755df024674efb0f0fc8
Andrew Ip <andrewip@andrewip.co.mentel.com>
2014-04-23 15:09:14 -0400

    footer hooks

--- footer.php
+++ footer.php
@@ -37,13 +37,13 @@
 					<h2 class="brand">Occasions</h2>
 
 					<ul>
-						<li><a href="#">Home</a></li>
+						<li><a href="/">Home</a></li>
 						<li><a href="#">About</a></li>
 						<li><a href="#">Careers</a></li>
 						<li><a href="#">Print Advertising</a></li>
 						<li><a href="#">Editorial</a></li>
 						<li><a href="#">Link to Us</a></li>
-						<li><a href="#">Purchase Copies</a></li>
+						<li><a href="/subscribe">Purchase Copies</a></li>
 						<li><a href="#">Support</a></li>
 						<li><a href="#">Privacy Policy</a></li>
 						<li><a href="#">Terms of Use</a></li>
@@ -57,21 +57,12 @@
 					<h2>Weddings</h2>
 
 					<ul>
-						<li><a href="#">Real Weddings</a></li>
-						<li><a href="#">Bridal Showers</a></li>
-						<li><a href="#">Rehearsal Dinners</a></li>
-						<li><a href="#">Destination Weddings</a></li>
-						<li><a href="#">Planning &amp; Advice</a></li>
-						<li><a href="#">Colors &amp; Themes</a></li>
-						<li><a href="#">Invitations &amp; Stationery</a></li>
-						<li><a href="#">Photography &amp; Videography</a></li>
-						<li><a href="#">Fashion &amp; Accessories</a></li>
-						<li><a href="#">Floral &amp; Decoration</a></li>
-						<li><a href="#">Cakes &amp; Dessert</a></li>
-						<li><a href="#">Catering &amp; Recipes</a></li>
-						<li><a href="#">Music &amp; Entertainment</a></li>
-						<li><a href="#">Rentals &amp; Event Design</a></li>
-						<li><a href="#">Venues &amp; Reception Sites</a></li>
+						<?php 
+							$cats = get_subcategories('weddings');
+							foreach($cats as $cat) {
+						?>
+						<li><a href="<?php echo get_category_link($cat->cat_ID); ?>"><?php echo $cat->name; ?></a></li>
+						<?php } ?>
 					</ul>
 
 				</li>
@@ -80,22 +71,12 @@
 					<h2>Parties &amp; Celebrations</h2>
 
 					<ul>
-						<li><a href="#">Birthday Parties</a></li>
-						<li><a href="#">Kids Parties</a></li>
-						<li><a href="#">For Girls</a></li>
-						<li><a href="#">Themes &amp; Ideas</a></li>
-						<li><a href="#">Baby Showers</a></li>
-						<li><a href="#">DIY &amp; Tutorials</a></li>
-						<li><a href="#">Themed Parties</a></li>
-						<li><a href="#">Real Mitzvahs</a></li>
-						<li><a href="#">Adult Parties</a></li>
-						<li><a href="#">Bridal Showers</a></li>
-						<li><a href="#">Sweet 16</a></li>
-						<li><a href="#">Quincea&Ntilde;eras</a></li>
-						<li><a href="#">For Boys</a></li>
-						<li><a href="#">For Business</a></li>
-						<li><a href="#">Anniversaries</a></li>
-						<li><a href="#">Decorations</a></li>
+						<?php 
+							$cats = get_subcategories('parties-and-celebrations');
+							foreach($cats as $cat) {
+						?>
+						<li><a href="<?php echo get_category_link($cat->cat_ID); ?>"><?php echo $cat->name; ?></a></li>
+						<?php } ?>
 					</ul>
 
 				</li>
@@ -104,16 +85,12 @@
 					<h2>Entertaining &amp; Holidays</h2>
 
 					<ul>
-						<li><a href="#">Food &amp; Drink</a></li>
-						<li><a href="#">Outdoor</a></li>
-						<li><a href="#">Dinner Parties</a></li>
-						<li><a href="#">Products</a></li>
-						<li><a href="#">Holiday Parties</a></li>
-						<li><a href="#">Decor &amp; Centerpieces</a></li>
-						<li><a href="#">Seasonal Settings</a></li>
-						<li><a href="#">Sports Themed</a></li>
-						<li><a href="#">Themes &amp; Ideas</a></li>
-						<li><a href="#">DIY &amp; Tutorials</a></li>
+						<?php 
+							$cats = get_subcategories('entertaining-and-holidays');
+							foreach($cats as $cat) {
+						?>
+						<li><a href="<?php echo get_category_link($cat->cat_ID); ?>"><?php echo $cat->name; ?></a></li>
+						<?php } ?>
 					</ul>
 
 					<h2>Local Editions</h2>
