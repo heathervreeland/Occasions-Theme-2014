f7a7e3606a91f0677290aebe80c85b2fdd266ac0
Andrew Ip <andrewip@andrewip.co.mentel.com>
2014-05-05 10:08:48 -0400

    checkout-layout

---
+++ inc/checkout-layout.php
@@ -0,0 +1,15 @@
+<div class="section-divider top-divider">
+	<div class="line"></div>
+	<span class="before">•</span>
+	<span class="heading">Checkout</span>
+	<span class="after">•</span>
+</div>
+
+<div class="after-divider">
+	<h2>Order Review</h2>
+	<?php
+
+		the_content();
+
+	?>
+</div>
