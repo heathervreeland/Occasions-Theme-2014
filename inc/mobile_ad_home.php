de991ff692e16a1b74768271e2e46e141aa874ed
Andrew Ip <andrewip@andrewip.co.mentel.com>
2014-05-12 16:15:48 -0400

    remove google text.

--- inc/mobile_ad_home.php
+++ inc/mobile_ad_home.php
@@ -1,7 +1,7 @@
 <?php 
 	
 	// Mobile detect 
-	echo do_shortcode("[phone]<div class=\"mobile_ad\">Google ad code is here: <script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>
+	echo do_shortcode("[phone]<div class=\"mobile_ad\"><script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>
 <!-- Home 320x50 mobile -->
 <ins class=\"adsbygoogle\" 
      style=\"display:inline-block;width:320px;height:50px\" 
