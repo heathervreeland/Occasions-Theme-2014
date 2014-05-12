b0a31d9e67405d9671abb2c717ab2581773c1923
Andrew Ip <andrewip@andrewip.co.mentel.com>
2014-05-12 16:20:50 -0400

    no glam ad on mobile

--- inc/side-top-ad.php
+++ inc/side-top-ad.php
@@ -1,7 +1,9 @@
 <?php if (oo_get_option('glam_ad')): ?>
+	<?php if( wpmd_is_notphone() ) : ?>
 	<div class="zone">
 		<div class="spot">
 			<script type="text/javascript" src="http://www2.glam.com/app/site/affiliate/viewChannelModule.act?mName=viewAdJs&amp;affiliateId=<?php oo_option('glam_ad') ?>&amp;adSize=300x250"></script>
 		</div>
 	</div>
+	<?php endif; ?>
 <?php endif ?>
