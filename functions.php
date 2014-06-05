0632069b17339f018e1824a1bfe9f61d1a99ae39
Andrew Ip <andrewip@andrewip.co.mentel.com>
2014-06-05 12:21:43 -0400

    make venue also have gallery

--- functions.php
+++ functions.php
@@ -802,4 +802,17 @@ function oo_comment($comment, $args, $depth) {
 <?php }
 
 
+
+
+/**
+ * Post types where metaboxes should show
+ * 
+ * @return array
+ */
+function flotheme_get_post_types_with_gallery() {
+	return array('post', 'venue');
+}
+
+
+
 ?>
