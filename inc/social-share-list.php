0e869915c95727372ca0125271e869666c4f47db
viviane gosselin <vgosselin@mentel.com>
2014-07-10 11:42:36 -0400

    Add Instagram and google+ icons

--- inc/social-share-list.php
+++ inc/social-share-list.php
@@ -2,5 +2,7 @@
 	<li><a href="<?php oo_option('fb');?>" class="facebook" target="_blank"></a></li>
 	<li><a href="http://twitter.com/#!/<?php oo_option('twi');?>" class="twitter" target="_blank"></a></li>
 	<li><a href="<?php oo_option('pin');?>" class="pinterest" target="_blank"></a></li>
+    <li><a href="http://Instagram.com/occasionsmag" class="instagram" target="_blank"></a></li>
+	<li><a href="https://plus.google.com/b/103715157874487467654/103715157874487467654/about<?php oo_option('twi');?>" class="google" target="_blank"></a></li>
 	<li><a href="<?php oo_rss(); ?>" class="rss" target="_blank"></a></li>
 </ul>
